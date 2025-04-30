0e8e0f3a55f57e24a9dacad995f43e2265ff321c
largelogs <163830200+largelogs@users.noreply.github.com>
2025-04-30 22:32:16 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+app.post('/verify-token', async (req, res) => {
+  const token = req.body.token;
+
+  try {
+    const response = await axios.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      null,
+      {
+        params: {
+          secret: SECRET_KEY,
+          response: token,
+        },
+      }
+    );
+
+    const data = response.data;
+    if (data.success && data.score > 0.5) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(403).json({ success: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend is running.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
