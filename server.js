f2d52224fd445ff85b129079b6566660f4277b17
largelogs <163830200+largelogs@users.noreply.github.com>
2025-05-01 02:42:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.set('trust proxy', 1);
+
 app.use(cors({ origin: 'https://depilout.com.br' }));
 app.use(express.json());
 app.use(morgan('tiny'));
