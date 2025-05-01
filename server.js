9665b2ca6758c21b115eec71332812e79c078113
largelogs <163830200+largelogs@users.noreply.github.com>
2025-05-01 02:37:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-const rateLimit = require('express-rate-limit');
-const morgan = require('morgan');
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(cors({
-  origin: 'https://depilout.com.br',
-}));
+app.use(cors({ origin: 'https://depilout.com.br' }));
 app.use(express.json());
 app.use(morgan('tiny'));
 
-// Optional rate limiting
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 30,
@@ -43,7 +42,7 @@ app.post('/verify-token', async (req, res) => {
     if (response.data.success && response.data.score > 0.5) {
       return res.json({
         success: true,
-        redirectUrl: 'https://sc.com' // Hidden from frontend code
+        redirectUrl: 'https://sc.com',
       });
     } else {
       return res.json({ success: false, message: 'CAPTCHA failed' });
